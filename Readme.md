c0a954a0bf2c065cf225329aac62f66406375d62
harshprajapati8347 <harshprajapati8347@gmail.com>
2022-11-28 17:02:23 +0530

    task for internhip

---
+++ Readme.md
@@ -0,0 +1,28 @@
+**Walkthrough** to [Word Frequency App.](https://hp-word-frequency.netlify.app/)
+
+- User **clicks submit** button to fetch data.
+
+- fetch data function fetches data.txt files and parsed them using the split() method. Then it created a map of words and their frequency. Then it sorted the map in descending order of frequency and displayed the top 20 words in a table.
+
+
+- recharts library was used to plot the histogram. The data was passed to the histogram as an array of objects with key words and frequencies. The histogram was plotted using BarChart, XAxis, YAxis and Bar components.
+
+- X-axis = top 20 words with highest occurrence
+- Y-axis = how many times (numer) they occurred in the file
+
+
+## How to run the project
+
+- Clone or download the project. 
+
+
+Open in Folder in Code Editor of your choise and then run the following commands in the project directory:
+
+- Requirements: Node.js, npm should be installed on your machine.
+1. `npm install`
+2. `npm start`
+
+- Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+## Technologies used
+- Reactjs, Javascript, HTML, CSS, Recharts, Netlify
---
+++ src/App.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect } from "react";
+import {
+  Tooltip,
+  BarChart,
+  XAxis,
+  YAxis,
+  Legend,
+  CartesianGrid,
+  Bar,
+} from "recharts";
+
+const App = () => {
+  const [data, setData] = React.useState();
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch("ttt.txt");
+      const data = await response.text();
+      const words = data.split(" ");
+      const wordCount = {};
+      words.forEach((word) => {
+        if (wordCount[word]) {
+          wordCount[word] += 1;
+        } else {
+          wordCount[word] = 1;
+        }
+      });
+      const sortedWordCount = Object.entries(wordCount).sort(
+        (a, b) => b[1] - a[1]
+      );
+      const top20 = sortedWordCount.slice(0, 20);
+      console.log(top20, "top20");
+      setData(top20);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={fetchData}>Submit</button>
+      {loading && <h1>Loading...</h1>}
+      {error && <div>{error}</div>}
+      {data && (
+        <BarChart
+          width={1000}
+          height={500}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis
+            dataKey={(data) => {
+              console.log(data, "data");
+              return data[0];
+            }}
+          />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar
+            dataKey={(data) => {
+              console.log(data, "data");
+              return data[1];
+            }}
+            fill="#534cdf"
+          />
+        </BarChart>
+      )}
+    </div>
+  );
+};
+
+export default App;
